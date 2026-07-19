32a26f2e1e7aad670c9fff04c34e300b5409e78d
lucas-wang-ece <lucaswjj1234@gmail.com>
2026-07-19 10:37:45 -0400

    Add comments to V4 threshold alert

--- src/threshold_alert/threshold_alert/threshold_alert.ino
+++ src/threshold_alert/threshold_alert/threshold_alert.ino
@@ -3,29 +3,29 @@
 #define DHT_PIN 2
 #define DHT_TYPE DHT11
 
-#define ALERT_LED_PIN LED_BUILTIN
-#define TEMPERATURE_THRESHOLD_C 25.0
+#define ALERT_LED_PIN LED_BUILTIN     // Use the built-in LED as the alert indicator.
+#define TEMPERATURE_THRESHOLD_C 25.0   //define threshold temperature
 
-DHT dht(DHT_PIN, DHT_TYPE);
+DHT dht(DHT_PIN, DHT_TYPE);     // Create a DHT sensor object using the selected pin and sensor type.
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(ALERT_LED_PIN, OUTPUT);
+  pinMode(ALERT_LED_PIN, OUTPUT);  //select the alert LED as the output pin
 
-  Serial.println("Embedded Environmental Data Logger");
+  Serial.println("Embedded Environmental Data Logger");  //print start up message
   Serial.println("V4: Threshold Alert Started");
 
-  dht.begin();
+  dht.begin();   //initialize the DHT11 sensor
 }
 
 void loop() {
   float humidity = dht.readHumidity();
   float temperatureC = dht.readTemperature();
 
-  if (isnan(humidity) || isnan(temperatureC)) {
+  if (isnan(humidity) || isnan(temperatureC)) {           //check if both inputs are valid
     Serial.println("Error: Failed to read from DHT11 sensor");
-    digitalWrite(ALERT_LED_PIN, LOW);
+    digitalWrite(ALERT_LED_PIN, LOW);               //if any of the input is invalid, turn off the ALERT LED
     delay(2000);
     return;
   }
@@ -38,12 +38,12 @@ void loop() {
   Serial.print(humidity);
   Serial.print(" %");
 
-  if (temperatureC >= TEMPERATURE_THRESHOLD_C) {
+  if (temperatureC >= TEMPERATURE_THRESHOLD_C) {        //check if surrounding temperature is higher than threshold temperature
     digitalWrite(ALERT_LED_PIN, HIGH);
-    Serial.println(" | Alert: ON");
+    Serial.println(" | Alert: ON");             //if yes, turn on the alert
   } else {
     digitalWrite(ALERT_LED_PIN, LOW);
-    Serial.println(" | Alert: OFF");
+    Serial.println(" | Alert: OFF");            //if no, turn off the alert
   }
 
   delay(2000);
