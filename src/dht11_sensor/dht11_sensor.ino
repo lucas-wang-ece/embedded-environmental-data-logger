9362f58ee66164ef3304154a886280244d3c623e
lucas-wang-ece <lucaswjj1234@gmail.com>
2026-07-19 10:32:01 -0400

    Add comments to V3 DHT11 sensor reading

--- src/dht11_sensor/dht11_sensor.ino
+++ src/dht11_sensor/dht11_sensor.ino
@@ -1,34 +1,34 @@
-#include "DHT.h"
+#include "DHT.h"            //include DHT library
 
-#define DHT_PIN 2
+#define DHT_PIN 2           //define constants and constant variables
 #define DHT_TYPE DHT11
 
-DHT dht(DHT_PIN, DHT_TYPE);             
+DHT dht(DHT_PIN, DHT_TYPE);        //create an dht class object with sensor type DHT11 and is connected to Digital 2 onn the board  
 
 void setup() {
   Serial.begin(9600);
 
-  Serial.println("Embedded Environmental Data Logger");
+  Serial.println("Embedded Environmental Data Logger");  //print startup message
   Serial.println("V3: DHT11 Sensor Reading Started");
 
-  dht.begin();
+  dht.begin();   //initialize DHT11 sensor
 }
 
 void loop() {
-  float humidity = dht.readHumidity();
-  float temperatureC = dht.readTemperature();
+  float humidity = dht.readHumidity();  //read humidity
+  float temperatureC = dht.readTemperature();  //read temperature
 
-  if (isnan(humidity) || isnan(temperatureC)) {
+  if (isnan(humidity) || isnan(temperatureC)) {       //check if any of the input is missing
     Serial.println("Error: Failed to read from DHT11 sensor");
     delay(2000);
     return;
   }
 
-  Serial.print("Temperature: ");
+  Serial.print("Temperature: ");   //print temperature
   Serial.print(temperatureC);
   Serial.print(" C");
 
-  Serial.print(" | Humidity: ");
+  Serial.print(" | Humidity: ");  //print humidity
   Serial.print(humidity);
   Serial.println(" %");
 
